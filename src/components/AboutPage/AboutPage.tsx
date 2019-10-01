05e9551ccf7502fa6b8edf5f4ca16b62eb17e0be
Artur Shestakov <shestakovartur2.0@gmail.com>
2019-10-01 17:57:38 +0500

    about page desctop

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import about from './about.module.scss'
+
+import person from '../../images/person.svg'
 
 const AboutPage: React.FC = () => {
 
     return(
-        <h1>AboutPage</h1>
+        <section className={about.aboutSection}>
+            <div className={about.helloBlock}>
+                <h2 className={about.helloBlock__message}>Hello, Smart Analytics</h2>
+                <div className={about.helloBlock__avatarContainer}>
+                    <img className={about.helloBlock__avatar} src={person} alt="person"></img>
+                </div>
+                
+            </div>
+            <h1 className={about.aboutSection__email}>shestakovartur@gmail.com</h1>
+        </section>
     )
 }
 
